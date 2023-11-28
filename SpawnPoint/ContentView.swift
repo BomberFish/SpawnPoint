500d1e40636d16c17a4a9637ee414e9d8b5287e5
BomberFish <bomberfish@bomberfish.ca>
2023-11-28 09:41:00 -0500

    Initial Commit

---
+++ SpawnPoint/ContentView.swift
@@ -0,0 +1,21 @@
+// bomberfish
+// ContentView.swift – SpawnPoint
+// created on 2023-11-28
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpawnPoint/SpawnPointApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// SpawnPointApp.swift – SpawnPoint
+// created on 2023-11-28
+
+import SwiftUI
+
+@main
+struct SpawnPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
